cddced41fe214d92f60eecec622238698b35f2e1
Cline, Caleb N CTR (USA) <caleb.n.cline.ctr@mail.mil>
2025-04-17 15:59:44 +0000

    Backend resource fix

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,7 +6,26 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+const schema = a.schema({
+  Todo: a.model({
+    content: a.string(),
+
+  }).authorization(allow => [allow.owner()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    // This tells the data client in your app (generateClient())
+    // to sign API requests with the user authentication token.
+
+    defaultAuthorizationMode: 'userPool',
+  },
+});
 
 // Export types for client-side code completion
 
